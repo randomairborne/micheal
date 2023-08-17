aa02b67bdc147e3963394fb6267985163ee8bc13
valkyrie_pilot <valk@randomairborne.dev>
2023-08-16 23:47:30 -0600

    initial micheal

---
+++ src/main.rs
@@ -0,0 +1,223 @@
+//! Requires the "client", "standard_framework", and "voice" features be enabled
+//! in your Cargo.toml, like so:
+//!
+//! ```toml
+//! [dependencies.serenity]
+//! git = "https://github.com/serenity-rs/serenity.git"
+//! features = ["client", "standard_framework", "voice"]
+//! ```
+use std::sync::Arc;
+
+use dashmap::DashMap;
+
+use serenity::{
+    async_trait,
+    client::{Client, Context, EventHandler},
+    framework::{
+        standard::{
+            macros::{command, group},
+            Args, CommandResult,
+        },
+        StandardFramework,
+    },
+    model::{channel::Message, gateway::Ready, id::ChannelId},
+    prelude::{GatewayIntents, Mentionable},
+    Result as SerenityResult,
+};
+
+use songbird::{
+    driver::DecodeMode,
+    model::{id::UserId, payload::Speaking},
+    Config, CoreEvent, Event, EventContext, EventHandler as VoiceEventHandler, SerenityInit,
+};
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[derive(Clone)]
+struct Receiver {
+    users: Arc<DashMap<u32, (Vec<i16>, UserId)>>,
+}
+
+impl Receiver {
+    pub fn new() -> Self {
+        // You can manage state here, such as a buffer of audio packet bytes so
+        // you can later store them in intervals.
+        Self {
+            users: Arc::new(DashMap::with_capacity(10)),
+        }
+    }
+}
+
+#[async_trait]
+impl VoiceEventHandler for Receiver {
+    #[allow(unused_variables)]
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        use EventContext as Ctx;
+        match ctx {
+            Ctx::SpeakingStateUpdate(Speaking {
+                speaking,
+                ssrc,
+                user_id: Some(user_id),
+                ..
+            }) => {
+                self.users
+                    .insert(*ssrc, (Vec::with_capacity(1_000_000), *user_id));
+            }
+            Ctx::VoiceTick(tick) => {
+                for (ssrc, data) in &tick.speaking {
+                    let Some(mut user) = self.users.get_mut(ssrc) else {
+                        continue;
+                    };
+                    if let Some(decoded_voice) = data.decoded_voice.as_ref() {
+                        let voice_len = decoded_voice.len();
+                        user.0.extend_from_slice(decoded_voice);
+                    } else {
+                        println!("ERROR: Decode disabled.");
+                    }
+                }
+                for i in self.users.iter() {
+                    let (ssrc, user) = i.pair();
+                    if !tick.speaking.contains_key(ssrc) {
+                        tokio::spawn(async { fire_request().await });
+                    }
+                }
+            }
+            _ => {
+                // We won't be registering this struct for any more event classes.
+            }
+        }
+
+        None
+    }
+}
+
+#[group]
+#[commands(join, leave)]
+struct General;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let config: AppConfig = envy::from_env().expect("Failed to read config");
+
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(|c| c.prefix("~"));
+
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+
+    // Here, we need to configure Songbird to decode all incoming voice packets.
+    // If you want, you can do this on a per-call basis---here, we need it to
+    // read the audio data that other people are sending us!
+    let songbird_config = Config::default().decode_mode(DecodeMode::Decode);
+
+    let mut client = Client::builder(&config.discord_token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .register_songbird_from_config(songbird_config)
+        .await
+        .expect("Err creating client");
+
+    let _ = client
+        .start()
+        .await
+        .map_err(|why| println!("Client ended: {:?}", why));
+}
+
+#[command]
+#[only_in(guilds)]
+async fn join(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
+    let connect_to = match args.single::<std::num::NonZeroU64>() {
+        Ok(id) => ChannelId(id),
+        Err(_) => {
+            check_msg(
+                msg.reply(ctx, "Requires a valid voice channel ID be given")
+                    .await,
+            );
+
+            return Ok(());
+        }
+    };
+
+    let guild_id = msg.guild_id.unwrap();
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+
+    if let Ok(handler_lock) = manager.join(guild_id, connect_to).await {
+        let mut handler = handler_lock.lock().await;
+
+        let evt_receiver = Receiver::new();
+
+        handler.add_global_event(CoreEvent::SpeakingStateUpdate.into(), evt_receiver.clone());
+        handler.add_global_event(CoreEvent::RtpPacket.into(), evt_receiver.clone());
+        handler.add_global_event(CoreEvent::RtcpPacket.into(), evt_receiver.clone());
+        handler.add_global_event(CoreEvent::ClientDisconnect.into(), evt_receiver.clone());
+        handler.add_global_event(CoreEvent::VoiceTick.into(), evt_receiver);
+
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, &format!("Joined {}", connect_to.mention()))
+                .await,
+        );
+    } else {
+        check_msg(
+            msg.channel_id
+                .say(&ctx.http, "Error joining the channel")
+                .await,
+        );
+    }
+
+    Ok(())
+}
+
+#[command]
+#[only_in(guilds)]
+async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
+    let guild_id = msg.guild_id.unwrap();
+
+    let manager = songbird::get(ctx)
+        .await
+        .expect("Songbird Voice client placed in at initialisation.")
+        .clone();
+    let has_handler = manager.get(guild_id).is_some();
+
+    if has_handler {
+        if let Err(e) = manager.remove(guild_id).await {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("Failed: {:?}", e))
+                    .await,
+            );
+        }
+
+        check_msg(msg.channel_id.say(&ctx.http, "Left voice channel").await);
+    } else {
+        check_msg(msg.reply(ctx, "Not in a voice channel").await);
+    }
+
+    Ok(())
+}
+
+/// Checks that a message successfully sent; if not, then logs why to stdout.
+fn check_msg(result: SerenityResult<Message>) {
+    if let Err(why) = result {
+        println!("Error sending message: {:?}", why);
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct AppConfig {
+    discord_token: String,
+    endpoint_token: String,
+    endpoint: String,
+}
